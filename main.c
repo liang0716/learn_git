c512e64e122da3850d57d19238d30eb1c7d16b8e
Leon <liang00716@gmail.com>
2022-09-07 01:59:41 +0800

    modify main.c add a for loop

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "power.c"
 
+#define size 5
+int pow_int(int base, int pow);
 int main(void)
 {
+    int i;
+
     printf("Hello hell!\n");
+    for(i = 0; i < size; i++)
+    {
+        printf(" pow(%d, 2) = %d\n", i, pow_int(i, 2));
+    }
     printf("\n");
     return 0;
 }
